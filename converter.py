c79294b7f8cefd83d85d7064ba395404c8ec522a
Maryam <71490610+mnajibi@users.noreply.github.com>
2023-11-04 17:39:25 -0400

    black formatter added, 4 file formatted

--- converter.py
+++ converter.py
@@ -1,23 +1,27 @@
 import os
 import re
-from  markdown_link_replacer import replace_links
+from markdown_link_replacer import replace_links
+
 
 class TxtConverter:
- @staticmethod
- def process_file_content(content):
-    lines = content.split("\n")
-    if len(lines) > 2 and lines[1] == "" and lines[2] == "":
-        title = lines[0].strip()
-        body_paragraphs = re.split(r"\r?\n\r?\n", "\n".join(lines[3:]))
-    else:
-        title = None
-        body_paragraphs = re.split(r"\r?\n\r?\n", content)
+    @staticmethod
+    def process_file_content(content):
+        lines = content.split("\n")
+        if len(lines) > 2 and lines[1] == "" and lines[2] == "":
+            title = lines[0].strip()
+            body_paragraphs = re.split(r"\r?\n\r?\n", "\n".join(lines[3:]))
+        else:
+            title = None
+            body_paragraphs = re.split(r"\r?\n\r?\n", content)
 
-    html_paragraphs = [f"<p>{p.strip()}</p>" for p in body_paragraphs if p.strip()]
-    return title, "\n".join(html_paragraphs)
- @staticmethod
- def create_html_from_txt(filepath, content=None, lang='en-CA', output_dir='./html/examples'):
-        with open(filepath, 'r') as f:
+        html_paragraphs = [f"<p>{p.strip()}</p>" for p in body_paragraphs if p.strip()]
+        return title, "\n".join(html_paragraphs)
+
+    @staticmethod
+    def create_html_from_txt(
+        filepath, content=None, lang="en-CA", output_dir="./html/examples"
+    ):
+        with open(filepath, "r") as f:
             content = f.read()
 
         # Parse and replace markdown links with valid ones
@@ -44,67 +48,72 @@ class TxtConverter:
 </html>"""
 
         os.makedirs(output_dir, exist_ok=True)
-        output_file = os.path.join(output_dir, os.path.basename(filepath).replace('.txt', '.html'))
-        with open(output_file, 'w') as f:
+        output_file = os.path.join(
+            output_dir, os.path.basename(filepath).replace(".txt", ".html")
+        )
+        with open(output_file, "w") as f:
             f.write(html_content)
 
+
 class MdConverter:
- @staticmethod
- def create_html_from_md(filepath, content=None, lang='en-CA' , output_dir='./html/examples'):
-    os.makedirs(output_dir, exist_ok=True)
+    @staticmethod
+    def create_html_from_md(
+        filepath, content=None, lang="en-CA", output_dir="./html/examples"
+    ):
+        os.makedirs(output_dir, exist_ok=True)
 
-    # with open(filepath, 'r', encoding='utf-8') as md_file:
-    #     markdown_content = md_file.read()
+        # with open(filepath, 'r', encoding='utf-8') as md_file:
+        #     markdown_content = md_file.read()
 
-    if content is None:
-        with open(filepath, 'r', encoding='utf-8') as md_file:
-            markdown_content = md_file.read()
-    else:
-        markdown_content = content
+        if content is None:
+            with open(filepath, "r", encoding="utf-8") as md_file:
+                markdown_content = md_file.read()
+        else:
+            markdown_content = content
 
-    # Use your functions to replace and validate markdown links
-        content, broken_links = replace_links(filepath, content, is_markdown=True)
+            # Use your functions to replace and validate markdown links
+            content, broken_links = replace_links(filepath, content, is_markdown=True)
 
-    # Here, you can handle `broken_links` if you wish, for example logging them
-        if broken_links:
-            print(f"Broken links found in {filepath}: {', '.join(broken_links)}")
+            # Here, you can handle `broken_links` if you wish, for example logging them
+            if broken_links:
+                print(f"Broken links found in {filepath}: {', '.join(broken_links)}")
 
-    lines = markdown_content.split('\n')
-    html_body_content = ""
-    current_paragraph = []
+        lines = markdown_content.split("\n")
+        html_body_content = ""
+        current_paragraph = []
 
-    for line in lines:
-     if line.startswith('# '):
-        # Close any open paragraph
+        for line in lines:
+            if line.startswith("# "):
+                # Close any open paragraph
+                if current_paragraph:
+                    html_body_content += "<p>" + " ".join(current_paragraph) + "</p>\n"
+                    current_paragraph = []
+                # Add the heading
+                html_body_content += f"<h1>{line[2:]}</h1>\n"
+            elif line.startswith("## "):
+                if current_paragraph:
+                    html_body_content += "<p>" + " ".join(current_paragraph) + "</p>\n"
+                    current_paragraph = []
+                html_body_content += f"<h2>{line[3:]}</h2>\n"
+            elif line.startswith("### "):
+                if current_paragraph:
+                    html_body_content += "<p>" + " ".join(current_paragraph) + "</p>\n"
+                    current_paragraph = []
+                html_body_content += f"<h3>{line[4:]}</h3>\n"
+            elif line.strip():
+                # Append non-empty lines to the current paragraph
+                current_paragraph.append(line)
+            else:
+                # If it's an empty line, close the current paragraph (if any)
+                if current_paragraph:
+                    html_body_content += "<p>" + " ".join(current_paragraph) + "</p>\n"
+                    current_paragraph = []
+
+            # Close any remaining open paragraph
         if current_paragraph:
-            html_body_content += '<p>' + ' '.join(current_paragraph) + '</p>\n'
-            current_paragraph = []
-        # Add the heading
-        html_body_content += f'<h1>{line[2:]}</h1>\n'
-     elif line.startswith('## '):
-        if current_paragraph:
-            html_body_content += '<p>' + ' '.join(current_paragraph) + '</p>\n'
-            current_paragraph = []
-        html_body_content += f'<h2>{line[3:]}</h2>\n'
-     elif line.startswith('### '):
-        if current_paragraph:
-            html_body_content += '<p>' + ' '.join(current_paragraph) + '</p>\n'
-            current_paragraph = []
-        html_body_content += f'<h3>{line[4:]}</h3>\n'
-     elif line.strip():
-        # Append non-empty lines to the current paragraph
-        current_paragraph.append(line)
-     else:
-        # If it's an empty line, close the current paragraph (if any)
-        if current_paragraph:
-            html_body_content += '<p>' + ' '.join(current_paragraph) + '</p>\n'
-            current_paragraph = []
+            html_body_content += "<p>" + " ".join(current_paragraph) + "</p>\n"
 
-     # Close any remaining open paragraph
-    if current_paragraph:
-      html_body_content += '<p>' + ' '.join(current_paragraph) + '</p>\n'
-
-    html_content = f"""<!DOCTYPE html>
+        html_content = f"""<!DOCTYPE html>
 <html lang="{lang}">
 <head>
   <meta charset="utf-8">
@@ -116,9 +125,11 @@ class MdConverter:
   {html_body_content}
 </body>
 </html>"""
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-    output_file = os.path.join(output_dir, os.path.basename(filepath).replace('.md', '.html'))
-    with open(output_file, 'w') as f:
-        f.write(html_content)
+        output_file = os.path.join(
+            output_dir, os.path.basename(filepath).replace(".md", ".html")
+        )
+        with open(output_file, "w") as f:
+            f.write(html_content)
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 #!/opt/anaconda3/bin/python
-#/usr/bin/env python3
+# /usr/bin/env python3
 import os
 import shutil
 from converter import TxtConverter, MdConverter
 from argparser import get_args
-from  markdown_link_replacer import replace_links
+from markdown_link_replacer import replace_links
 
 
 def process_file(file_path, lang, output_dir):
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         content = f.read()
-    
-    if file_path.endswith('.txt'):
-        updated_content, broken_links = replace_links(file_path, content, is_markdown=False)
+
+    if file_path.endswith(".txt"):
+        updated_content, broken_links = replace_links(
+            file_path, content, is_markdown=False
+        )
         TxtConverter.create_html_from_txt(file_path, updated_content, lang, output_dir)
-    elif file_path.endswith('.md'):
-        with open(file_path, 'r', encoding='utf-8') as f:
+    elif file_path.endswith(".md"):
+        with open(file_path, "r", encoding="utf-8") as f:
             content = f.read()
         # Save the updated_content back to file or handle it as needed
-        updated_content, broken_links = replace_links(file_path, content, is_markdown=True)
+        updated_content, broken_links = replace_links(
+            file_path, content, is_markdown=True
+        )
         # Handle broken_links as needed, for example, print a warning or error message
         if broken_links:
             print(f"Broken links found in {file_path}: {broken_links}")
         MdConverter.create_html_from_md(file_path, updated_content, lang, output_dir)
+
 
 def main():
     args = get_args()
@@ -44,7 +49,6 @@ def main():
     if args.lang:
         print(f"Language set to: {args.lang}")
 
-
     # Handle file or directory processing
     if os.path.isfile(args.path):
         process_file(args.path, args.lang, args.output)
@@ -56,5 +60,5 @@ def main():
                 process_file(os.path.join(root, file), args.lang, args.output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
