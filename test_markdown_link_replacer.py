4b0008395ab6f2f48351d5112842e19eb82edcf6
Maryam <71490610+mnajibi@users.noreply.github.com>
2023-11-08 13:33:37 -0500

    Add passing test case for replace_links function

---
+++ test_markdown_link_replacer.py
@@ -0,0 +1,21 @@
+# Import pytest
+import pytest
+
+
+from markdown_link_replacer import replace_links
+
+
+# Here's an example of a pytest function to test replace_links with good values
+def test_replace_links_good():
+    # setup input data and expected output
+    input_data = "Test content with a [link](https://www.google.com)"
+    expected_output = 'Test content with a <a href="https://www.google.com">link</a>'
+    # call replace_links
+    output, broken_links = replace_links("test.md", input_data, True)
+    # assert to check the output against the expected result
+    assert (
+        output == expected_output
+    ), "replace_links did not return expected HTML content"
+
+
+# Add more tests below for different conditions, such as bad values, null inputs, etc.
