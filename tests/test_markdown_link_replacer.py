314b0cdcd96b99e5d883b51cf08564494f4c15e1
Rachit1313 <Rachitchawla33@gmail.com>
2023-11-16 21:54:37 -0500

    added 2 tests for markdown replace_links function

--- tests/test_markdown_link_replacer.py
+++ tests/test_markdown_link_replacer.py
@@ -16,3 +16,19 @@ def test_replace_links_good():
     assert (
         output == expected_output
     ), "replace_links did not return expected HTML content"
+
+# Test replace_links with a broken link in markdown content
+def test_replace_links_broken_link_markdown():
+    input_data = "Test content with a [broken link](http://example.com/broken)"
+    expected_output = 'Test content with a <a href="#" style="color:red;">broken link</a>'
+    output, broken_links = replace_links("test.md", input_data, True)
+    assert output == expected_output
+    assert broken_links == ["http://example.com/broken"]
+
+# Test replace_links with valid link in plain text content
+def test_replace_links_valid_link_plain_text():
+    input_data = "This is a valid link: http://www.valid-link.com"
+    expected_output = 'This is a valid link: <a href="http://www.valid-link.com">http://www.valid-link.com</a>'
+    output, broken_links = replace_links("test.txt", input_data, False)
+    assert output == expected_output
+    assert broken_links == []
